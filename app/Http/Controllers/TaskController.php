b854d29aa09697b541bf76cf8a3f342a89738a28
deo <deoninja@gmail.com>
2024-02-21 15:57:57 +0800

    create route, model resource

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TaskCollection;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        // return response()->json(Task::all());
+        return new TaskCollection(Task::all());
+    }
+}
