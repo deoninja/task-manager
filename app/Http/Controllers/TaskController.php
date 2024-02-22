32c3b9c4d3abde906be8815db7b3cd01e886a3ff
deo <deoninja@gmail.com>
2024-02-22 11:48:26 +0800

    Filter Resource Collections

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,13 +8,17 @@ use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class TaskController extends Controller
 {
     public function index(Request $request)
     {
         // return response()->json(Task::all());
-        return new TaskCollection(Task::paginate());
+        $task = QueryBuilder::for(Task::class)
+            ->allowedFilters('is_done')
+            ->paginate();
+        return new TaskCollection($task);
     }
 
     public function show(Request $request, Task $task)
